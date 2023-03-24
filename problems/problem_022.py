73789929b13460a7989e8262bc4e1ef910b73a2a
Samantha <samanthachin.dsp@gmail.com>
2023-03-24 15:29:12 -0700

    Solved Problem 022

--- problems/problem_022.py
+++ problems/problem_022.py
@@ -7,4 +7,22 @@
 #     "surfboard"
 
 def gear_for_day(is_workday, is_sunny):
-    pass
+    # Create empty list to store gear for the day
+    gear = []
+    if is_sunny == "Y" and is_workday == "Y":
+        gear.append("umbrella")
+        gear.append("laptop")
+    elif is_workday == "Y":
+        gear.append("laptop")
+    elif is_sunny == "Y" and is_workday == "N":
+        gear.append("surfboard")
+    else:
+        print("No gear is needed for today!")
+
+    print(gear)
+
+# Test Cases:
+# gear_for_day(is_workday = "Y", is_sunny = "N")
+# gear_for_day(is_workday = "N", is_sunny = "N")
+# gear_for_day(is_workday = "Y", is_sunny = "Y")
+# gear_for_day(is_workday = "N", is_sunny = "Y")
