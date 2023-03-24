d7e25606cdee07e9d0273d3ce13071ce2fcca9c0
Samantha <samanthachin.dsp@gmail.com>
2023-03-24 11:55:37 -0700

    Solved Problem 006

--- problems/problem_006.py
+++ problems/problem_006.py
@@ -10,4 +10,15 @@
 # problem to get a good feel for how to solve it.
 
 def can_skydive(age, has_consent_form):
-    pass
+    # * The person must be greater than or equal to 18 years old, or
+    # * The person must have a signed consent form
+    if age >= 18 or has_consent_form is True:
+        print("You can go skydiving!")
+    else:
+        print("You cannot go skydiving.")
+
+# Test case:
+# can_skydive(17,False)
+# can_skydive(18,False)
+# can_skydive(40,True)
+# can_skydive(12,True)
