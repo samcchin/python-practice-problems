74788bf5e0569a391e82b581b9877f9a8ccdaa00
Samantha <samanthachin.dsp@gmail.com>
2023-03-24 14:57:41 -0700

    Solved Problem 025

--- problems/problem_025.py
+++ problems/problem_025.py
@@ -5,6 +5,17 @@
 # If the list of values is empty, the function should
 # return None
 #
-
 def calculate_sum(values):
-    pass
+    # If the list of values is empty, return None
+    if values == []:
+        return None
+    else:
+        # Set the sum counter to 0 and iterate through list to add num
+        sum = 0
+        for num in values:
+            sum += num
+        print(sum)
+
+# # Test Cases:
+# calculate_sum([1, 2, 3])
+# calculate_sum([5,10,20])
