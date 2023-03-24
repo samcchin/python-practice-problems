db8a009873609406f4cf6cffd944e635c7f42005
Samantha <samanthachin.dsp@gmail.com>
2023-03-24 15:07:14 -0700

    Solved Problem 027

--- problems/problem_027.py
+++ problems/problem_027.py
@@ -5,4 +5,12 @@
 #
 
 def max_in_list(values):
-    pass
+    if values == []:
+        return None
+    else:
+        print(max(values))
+
+# Test Cases
+max_in_list([22, 24, 26, 900])
+max_in_list([0])
+max_in_list([1.4, 5, 601])
