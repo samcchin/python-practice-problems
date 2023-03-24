35214aeb5c48b49b2bcd9f6aa9e6cc35eafa028f
Samantha <samanthachin.dsp@gmail.com>
2023-03-24 15:02:55 -0700

    Solved Problem 026

--- problems/problem_026.py
+++ problems/problem_026.py
@@ -13,4 +13,24 @@
 #   * An "F" for any other average
 
 def calculate_grade(values):
-    pass
+    sum = 0
+    for scores in values:
+        sum += scores
+    average = sum / len(values)
+
+    if average >= 90:
+        print("A")
+    elif average < 90 and average >= 80:
+        print("B")
+    elif average < 80 and average >= 70:
+        print("C")
+    elif average < 70 and average >= 60:
+        print("D")
+    else:
+        print("F")
+
+# #Test Cases
+# calculate_grade([80, 80, 80])
+# calculate_grade([60, 30, 90, 80,100])
+# calculate_grade([90,80,100])
+# calculate_grade([70, 65, 45, 55])
