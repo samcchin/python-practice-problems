fa41b4ba1683abeda1cb8f33592cd52d0e84cc18
Samantha <samanthachin.dsp@gmail.com>
2023-03-24 12:17:13 -0700

    Solved Problem 012

--- problems/problem_012.py
+++ problems/problem_012.py
@@ -16,4 +16,23 @@
 # problem to get a good feel for how to solve it.
 
 def fizzbuzz(number):
-    pass
+    # Set condition The word "fizzbuzz" if number is evenly divisible by
+    # both 3 and 5
+    if number % 3 == 0 and number % 5 == 0:
+        print("fizzbuzz")
+    # Set condition The word "fizz" if number is evenly divisible by 3
+    elif number % 3 == 0:
+        print("fizz")
+    # Set condition The word "buzz" if number is evenly divisible by 5
+    elif number % 5 == 0:
+        print("buzz")
+    # Set condition to print num if neither divisble by 3 or 5
+    else:
+        print(number)
+
+# Test Cases
+# fizzbuzz(3)
+# fizzbuzz(15)
+# fizzbuzz(5)
+# fizzbuzz(298)
+# fizzbuzz(1)
