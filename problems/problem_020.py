b838832e81bbeacd5daa5a356048fe2d70257cf9
Samantha <samanthachin.dsp@gmail.com>
2023-03-24 13:03:06 -0700

    Solved Problem 020

--- problems/problem_020.py
+++ problems/problem_020.py
@@ -3,4 +3,15 @@
 # to 50% of the number of people in the members list.
 
 def has_quorum(attendees_list, members_list):
-    pass
+    # Define condition if quorum has been met
+    if attendees_list > (members_list * .5):
+        print("Quorum has been met.")
+    # Defoine condition if quorum has not been met
+    else:
+        print("Quorum has not been met based on the number of attendees.")
+
+# # Test Cases
+# has_quorum(24, 100)
+# has_quorum(4, 10)
+# has_quorum(5, 20)
+# has_quorum(240, 300)
