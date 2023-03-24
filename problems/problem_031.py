71d8bbf01887899a23c9777a72087ba4b9f57a12
Samantha <samanthachin.dsp@gmail.com>
2023-03-24 16:16:19 -0700

    Solved Problem 031

--- problems/problem_031.py
+++ problems/problem_031.py
@@ -14,4 +14,23 @@
 # problem to get a good feel for how to solve it.
 
 def sum_of_squares(values):
-    pass
+    if values == []:
+        print(None)
+    else:
+        sum = 0
+        for num in values:
+            squared_num = num ** 2
+            sum += squared_num
+        print(sum)
+
+# Test Cases:
+# sum_of_squares([1,2,3,4])
+# sum_of_squares([0,2,1,0])
+# sum_of_squares([1,1,1,1])
+# sum_of_squares([0])
+# sum_of_squares([0])
+
+# Return None if the list is empty
+# Loop through each num in the list and square it
+# Add the result of each squared value
+# Return an integer value
