97223d5a264162c8eb30965e116856566730da12
Samantha <samanthachin.dsp@gmail.com>
2023-03-24 11:48:00 -0700

    Solved Problem 004

--- problems/problem_004.py
+++ problems/problem_004.py
@@ -13,4 +13,17 @@
 # problem to get a good feel for how to solve it.
 
 def max_of_three(value1, value2, value3):
-    pass
+    if value1 == value2 == value3:
+        print(value1 or value2 or value3)
+    elif value1 >= value2 and value1 >= value3:
+        print(value1)
+    elif value2 >= value1 and value2 >= value3:
+        print(value2)
+    else:
+        print(value3)
+
+#Test Case:
+# max_of_three(7,5,7)
+# max_of_three(1,2,3)
+# max_of_three(9,4,7)
+# max_of_three(16,2330.5,9)
