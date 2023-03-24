68487e3cd11bd041ef2d9ffaa8730628ef0fba92
Samantha <samanthachin.dsp@gmail.com>
2023-03-24 15:47:14 -0700

    Solved Problem 030

--- problems/problem_030.py
+++ problems/problem_030.py
@@ -12,4 +12,19 @@
 # problem to get a good feel for how to solve it.
 
 def find_second_largest(values):
-    pass
+    if values == []:
+        print("None")
+    elif len(values) == 1:
+        print("None")
+    else:
+        sorted_list = sorted(values)
+        second_largest_index = len(sorted_list)-2
+        second_largest_num = sorted_list[second_largest_index]
+        print(second_largest_num)
+
+# Test Cases:
+# find_second_largest([1])
+# find_second_largest([1,2,4,5,6,4])
+# find_second_largest([1,2,4,5,6,4,3,2])
+# find_second_largest([])
+# find_second_largest([3,2,1,4])
