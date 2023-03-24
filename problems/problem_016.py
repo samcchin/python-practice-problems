8f4d8b53dbfb4b507d9e5924210230bbaf09f9d3
Samantha <samanthachin.dsp@gmail.com>
2023-03-24 12:46:32 -0700

    Solved Problem 016

--- problems/problem_016.py
+++ problems/problem_016.py
@@ -3,4 +3,15 @@
 # make sure they're between 0 and 10, inclusive.
 
 def is_inside_bounds(x, y):
-    pass
+    # Ensure x and y coordinate are both within 0 and 10
+    if x >= 0 and x <= 10 and y >= 0 and y <= 10:
+        # return x, y is True
+        print("Is within bounds")
+    else:
+        # return x, y is False
+        print("Is outside bounds")
+
+# Test Cases
+# is_inside_bounds(2,3)
+# is_inside_bounds(21,3)
+# is_inside_bounds(0,10)
