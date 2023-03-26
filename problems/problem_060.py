2a0bba9e90878c81d4db498fa8d08ed0c7071e95
Samantha <samanthachin.dsp@gmail.com>
2023-03-25 22:23:39 -0700

    Solved Problem 060

--- problems/problem_060.py
+++ problems/problem_060.py
@@ -13,3 +13,14 @@
 #     * input:   [1, 3, 5, 7]
 #       returns: [1, 3, 5, 7]
 
+def only_odds(list):
+    odd_list = []
+    for num in list:
+        if num % 2 == 1:
+            odd_list.append(num)
+    print(odd_list)
+
+# # Test Case
+# only_odds([1, 2, 3, 4])
+# only_odds([2, 4, 6, 8])
+# only_odds([1, 3, 5, 7])
