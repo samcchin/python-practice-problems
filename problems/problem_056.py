f373cb22e2e7bd0682b93eb82434a261ee04d1bf
Samantha <samanthachin.dsp@gmail.com>
2023-03-25 22:37:53 -0700

    Solved Problem 056

--- problems/problem_056.py
+++ problems/problem_056.py
@@ -15,3 +15,13 @@
 #       parameter 2: 0
 #     returns: "92380"
 
+
+def num_concat(num1, num2):
+    result = str(num1) + str(num2)
+    print(result)
+
+# Test cases
+# num_concat(9238,0)
+# num_concat(10,3)
+# num_concat(45, 29)
+# num_concat(415, 2239)
