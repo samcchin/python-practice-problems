dde2a8fc715a931b42927f08fcd547bf80ac2d8f
Samantha <samanthachin.dsp@gmail.com>
2023-03-27 11:37:06 -0700

    Solved Problem 070

--- problems/problem_070.py
+++ problems/problem_070.py
@@ -19,6 +19,19 @@
 # Do it without pseudocode, this time, from memory. Don't look
 # at the last one you just wrote unless you really must.
 
+class Book:
+    def __init__(book, author, title):
+        book.author = author
+        book.title = title
+
+    def get_author(book):
+        return "Author: " + book.author
+
+    def get_title(book):
+        return "Title: " + book.title
 
 
+book = Book("Natalie Zina Walschots", "Hench")
 
+print(book.get_author())  # prints "Author: Natalie Zina Walschots"
+print(book.get_title())   # prints "Title: Hench"
