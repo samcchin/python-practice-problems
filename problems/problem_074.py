de653482d0bd7ee0945a3d0f3e4abae83ce27dbd
Samantha <samanthachin.dsp@gmail.com>
2023-03-27 12:17:20 -0700

    Solved Problem 074

--- problems/problem_074.py
+++ problems/problem_074.py
@@ -21,15 +21,40 @@
 #
 # There is pseudocode for you to guide you.
 
+class BankAccount:
+    def __init__(account, balance):
+        account.balance = balance
+
+    def get_balance(account):
+        return account.balance
+
+    def withdraw(account, amount):
+        account.balance -= amount
+        return account.balance
+
+    def deposit(account, amount):
+        account.balance += amount
+        return account.balance
+
+
+# Test Example:
+account = BankAccount(100)
+
+print(account.get_balance())  # prints 100
+account.withdraw(50)
+print(account.get_balance())  # prints 50
+account.deposit(120)
+print(account.get_balance())  # prints 170
+
 # class BankAccount
-    # method initializer(self, balance)
-        # self.balance = balance
+# method initializer(self, balance)
+#     self.balance = balance
 
-    # method get_balance(self)
-        # returns the balance
+# method get_balance(self)
+#     returns the balance
 
-    # method withdraw(self, amount)
-        # reduces the balance by the amount
+# method withdraw(self, amount)
+#     reduces the balance by the amount
 
-    # method deposit(self, amount)
-        # increases the balance by the amount
+# method deposit(self, amount)
+#     increases the balance by the amount
