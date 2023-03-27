882ef6bcacc888feb1e5b0a82db79d19bfce3800
Samantha <samanthachin.dsp@gmail.com>
2023-03-27 11:18:52 -0700

    Solved Problem 067

--- problems/problem_067.py
+++ problems/problem_067.py
@@ -22,16 +22,33 @@
 #
 # There is pseudocode availabe for you to guide you
 
-
 # class Employee
     # method initializer method with required state
     # parameters first name and last name
         # set self.first_name = first_name
         # set self.last_name = last_name
 
-    # method get_fullname(self)
-        # returns self.first_name + " " + self.last_name
+class Employee:
+    def __init__(employee, first_name, last_name):
+        employee.first_name = first_name
+        employee.last_name = last_name
 
-    # method get_email(self)
-        # returns self.first_name.lower() + "." + self.last_name.lower()
-        #         + "@company.com"
+    def get_fullname(employee):
+        return employee.first_name + " " + employee.last_name
+
+    def get_email(employee):
+        return employee.first_name.lower() + "." + employee.last_name.lower() + "@company.com"
+
+
+# Test Example:
+employee = Employee("Duska", "Ruzicka")
+
+print(employee.get_fullname())  # prints "Duska Ruzicka"
+print(employee.get_email())     # prints "duska.ruzicka@company.com"
+
+# method get_fullname(self)
+# returns self.first_name + " " + self.last_name
+
+# method get_email(self)
+# returns self.first_name.lower() + "." + self.last_name.lower()
+#         + "@company.com"
