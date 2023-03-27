97de9d816b79702ddf8b55558f95831a0a12c131
Samantha <samanthachin.dsp@gmail.com>
2023-03-27 12:38:18 -0700

    Solved Problem 078

--- problems/problem_078.py
+++ problems/problem_078.py
@@ -6,7 +6,8 @@
 #    * radius, a non-negative value
 #
 # Behavior:
-#    * calculate_perimeter()  # Returns the length of the perimater of the circle
+#    * calculate_perimeter()  # Returns the length of the perimater of the
+# circle
 #    * calculate_area()       # Returns the area of the circle
 #
 # Example:
@@ -21,4 +22,30 @@
 import math
 
 
+class Circle:
+    def __init__(circle, radius):
+        if radius < 0:
+            raise ValueError
+        else:
+            circle.radius = radius
 
+    def calculate_perimeter(circle):
+        perimeter = 2 * math.pi * circle.radius
+        return perimeter
+
+    def calculate_area(circle):
+        area = math.pi * (circle.radius ** 2)
+        return area
+
+
+# Test Example:
+circle = Circle(10)
+
+print(circle.calculate_perimeter())  # Prints 62.83185307179586
+print(circle.calculate_area())       # Prints 314.1592653589793
+
+#     method calculate_perimeter(self)
+#         returns 2 * math.pi * self.radius
+
+#     method calculate_area(self)
+#         returns math.pi * (self.radius squared)
