19d43fc663989c78e94f6ac6053e8d332200e8a9
Samantha <samanthachin.dsp@gmail.com>
2023-03-27 12:12:48 -0700

    Solved Problem 073

--- problems/problem_073.py
+++ problems/problem_073.py
@@ -22,6 +22,32 @@
 # Do it without pseudocode, this time, from memory. Don't look
 # at the last one you just wrote unless you really must.
 
+class Student:
+    def __init__(student, name):
+        student.name = name
+        student.scores = []
+
+    def add_score(student, score):
+        student.scores.append(score)
+
+    def get_average(student):
+        if student.scores == []:
+            return None
+        else:
+            sum_of_grades = 0
+            count = len(student.scores)
+            for grade in student.scores:
+                sum_of_grades += grade
+            average = sum_of_grades / count
+            return int(average)
 
 
+# Test Example:
+student = Student("Malik")
 
+print(student.get_average())    # Prints None
+student.add_score(80)
+print(student.get_average())    # Prints 80
+student.add_score(90)
+student.add_score(82)
+print(student.get_average())    # Prints 84
