6bc3d3ee84623f9308fd500e1d70216a8780c48e
Samantha <samanthachin.dsp@gmail.com>
2023-03-27 15:46:16 -0700

    Solved Problem 061

--- problems/problem_061.py
+++ problems/problem_061.py
@@ -14,3 +14,15 @@
 #     * input:   [1, 3, 3, 20, 3, 2, 2]
 #       returns: [1, 3, 20, 2]
 
+def remove_duplicates(list_of_values):
+    deduped_list = []
+    for item in list_of_values:
+        if item not in deduped_list:
+            deduped_list.append(item)
+    return deduped_list
+
+
+# Test cases:
+print(remove_duplicates([1, 1, 1, 1]))
+print(remove_duplicates([1, 2, 2, 1]))
+print(remove_duplicates([1, 3, 3, 20, 3, 2, 2]))
