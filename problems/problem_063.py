6a5892b2a7ef4890d7ca921653d3d81680e6fa18
Samantha <samanthachin.dsp@gmail.com>
2023-03-28 13:02:12 -0700

    Solved Problem 063

--- problems/problem_063.py
+++ problems/problem_063.py
@@ -21,3 +21,21 @@
 # You may want to look at the built-in Python functions
 # "ord" and "chr" for this problem
 
+def shift_letters(word):
+    new_word = ""
+    for letter in word:
+        if letter == "Z":
+            new_letter = "A"
+        elif letter == "z":
+            new_letter = "a"
+        else:
+            new_letter = chr(ord(letter)+1)
+        new_word += new_letter
+    return new_word
+
+
+# Test Case:
+print(shift_letters("import"))
+print(shift_letters("ABBA"))
+print(shift_letters("Kala"))
+print(shift_letters("zap"))
