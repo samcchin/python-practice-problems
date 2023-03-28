378db36f63a6656616076870fb27888fac1a9126
Samantha <samanthachin.dsp@gmail.com>
2023-03-27 17:48:15 -0700

    Solved Problem 050

--- problems/problem_050.py
+++ problems/problem_050.py
@@ -12,3 +12,14 @@
 #    * input: [1, 2, 3]
 #      result: [1, 2], [3]
 
+def halve_the_list(list):
+    half_list_length = int(len(list)/2)
+    if len(list) % 2 == 0:
+        return list[:half_list_length], list[half_list_length:]
+    else:
+        return list[:half_list_length+1], list[half_list_length+1:]
+
+
+# Test Case:
+print(halve_the_list([1, 2, 3, 4]))
+print(halve_the_list([1, 2, 3]))
