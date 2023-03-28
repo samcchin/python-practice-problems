e2a61ba8ccd296d6b6a4aad951976119fb37bace
Samantha <samanthachin.dsp@gmail.com>
2023-03-28 12:21:55 -0700

    Solved Problem 057

--- problems/problem_057.py
+++ problems/problem_057.py
@@ -13,3 +13,14 @@
 #     * input:   3
 #       returns: 1/2 + 2/3 + 3/4
 
+def sum_fraction_sequence(num):
+    sum = 0
+    for i in range(1, num+1):
+        sum += i / (i + 1)
+    return sum
+
+
+# Test Case:
+print(sum_fraction_sequence(1))
+print(sum_fraction_sequence(2))
+print(sum_fraction_sequence(3))
